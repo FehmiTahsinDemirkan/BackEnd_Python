c4ff9a8ddcc5a41f6d2108e99ce979ac21b61bde
Fehmi Tahsin Demirkan <fehmitahsindemirkan@gmail.com>
2025-01-16 12:21:45 +0300

    for_while

---
+++ Loops and Iterations/for_while.py
@@ -0,0 +1,26 @@
+nums=[1,2,3,4,5]
+
+# for num in nums:
+#     if num==3:
+#         print('Found')
+#         continue # break donguyu sonlandirio
+#     print(num)
+
+# for num in nums:
+#     for letter in 'abc':
+#         print(num,letter)
+
+# for i in range(10):
+#     print(i)
+
+# x =11
+# while x>10:
+#     if x==976483:
+#         break
+#     print(x)
+#     x+=1
+#
+# x=0
+# while True:
+#     print(x)
+#     x+=1
