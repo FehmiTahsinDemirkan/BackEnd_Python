b079feb3d0b03f9601f8c1e5b81651a6f4fe43b2
Fehmi Tahsin Demirkan <fehmitahsindemirkan@gmail.com>
2025-01-19 00:22:06 +0300

    Import and OS module added

---
+++ Import Modules/intro.py
@@ -0,0 +1,35 @@
+from my_modules import find_index,test
+# import my_modules as pp
+import sys
+courses=['Python','Java','C++','C','C#']
+
+index = find_index(courses,'Python')
+# print(index)
+# print(test)
+
+# print(sys.path)
+
+import  random
+
+random_course = random.choice(courses)
+# print(random_course)
+
+
+import math
+rads = math.radians(90)
+# print(rads)
+
+
+import datetime
+import calendar
+
+today = datetime.date.today()
+# print(today)
+# print(calendar.month(today.year,today.month))
+
+import os
+# print(os.getcwd())
+# print(os.__file__)
+
+import antigravity
+print(antigravity)
---
+++ OS Module/osModule.py
@@ -0,0 +1,10 @@
+import os
+# os.makedirs('OS-Demo-2/Sub-Folder')
+# os.removedirs('OS-Demo-2/Sub-Folder')
+# print(os.listdir())
+# os.chdir('C:\Mindsite-Nucleus')
+# os.rename('yarn.lock','test.lock')
+# print(os.environ.get('Users'))
+
+path = os.environ.get('C:\Mindsite-Nucleus\nucleus')
+print(path)
